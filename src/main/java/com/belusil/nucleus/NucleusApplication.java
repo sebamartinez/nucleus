41181e76f33b58a302ff6ddad0d6dda94fc70eab
Sebastian Martinez <sebastianmartinez@softwaremissinglink.com>
2019-06-11 00:13:03 -0300

    Initial Spring boot configuration

---
+++ src/main/java/com/belusil/nucleus/NucleusApplication.java
@@ -0,0 +1,13 @@
+package com.belusil.nucleus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NucleusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NucleusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/belusil/nucleus/NucleusApplicationTests.java
@@ -0,0 +1,16 @@
+package com.belusil.nucleus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NucleusApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
